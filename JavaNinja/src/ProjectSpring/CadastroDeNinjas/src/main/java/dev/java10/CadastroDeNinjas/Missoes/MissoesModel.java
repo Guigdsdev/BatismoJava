54a6357b2355409ecacc25a7fff0a45d8984d2e3
Guigdsdev <guigdsdev@gmail.com>
2025-10-24 10:49:32 -0300

    feat: Coloca lombok na classe para abstração

--- JavaNinja/src/ProjectSpring/CadastroDeNinjas/src/main/java/dev/java10/CadastroDeNinjas/Missoes/MissoesModel.java
+++ JavaNinja/src/ProjectSpring/CadastroDeNinjas/src/main/java/dev/java10/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10.CadastroDeNinjas.Missoes;
 
 import dev.java10.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,32 +24,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     List<NinjaModel> ninjas;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String nomeMissao, String dificuldadeMissao) {
-        this.id = id;
-        this.nomeMissao = nomeMissao;
-        this.dificuldadeMissao = dificuldadeMissao;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNomeMissao() {
-        return nomeMissao;
-    }
-
-    public void setNomeMissao(String nomeMissao) {
-        this.nomeMissao = nomeMissao;
-    }
-
-    public String getDificuldadeMissao() {
-        return dificuldadeMissao;
-    }
-
-    public void setDificuldadeMissao(String dificuldadeMissao) {
-        this.dificuldadeMissao = dificuldadeMissao;
-    }
 }
