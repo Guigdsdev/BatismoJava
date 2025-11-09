8fd6c2c215253144e584b64ebf534b394b28aeca
Guigdsdev <guigdsdev@gmail.com>
2025-11-09 01:43:54 -0300

    feat: Adicionado rota de UI para Ninjas e Missoes

--- JavaNinja/src/ProjectSpring/CadastroDeNinjas/src/main/java/dev/java10/CadastroDeNinjas/Missoes/MissoesControllerUi.java
+++ JavaNinja/src/ProjectSpring/CadastroDeNinjas/src/main/java/dev/java10/CadastroDeNinjas/Missoes/MissoesControllerUi.java
@@ -1,4 +1,47 @@
 package dev.java10.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/missao/ui")
 public class MissoesControllerUi {
+    private final MissoesService missoesService;
+
+    public MissoesControllerUi(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
+    @GetMapping("/listar")
+    public String listarMissao(Model model){
+        List<MissoesDTO> missoes = missoesService.listarMissoes();
+        model.addAttribute("missoes", missoes);
+        return "listarMissoes";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarMissao(@PathVariable Long id){
+        missoesService.deletarMissao(id);
+        return "redirect:/missao/ui/listar";
+    }
+
+    @GetMapping("/adicionar")
+    public String criarMissaoPagina(Model model){
+        model.addAttribute("missao", new MissoesDTO());
+        return "criarMissao";
+    }
+
+    @PostMapping("/salvar")
+    public String criarMissao(MissoesDTO missao, RedirectAttributes redirectAttributes){
+        missoesService.adicionarMissao(missao);
+        redirectAttributes.addFlashAttribute("mensagem", "Missão criada com sucesso");
+        return "redirect:/missao/ui/listar";
+    }
 }
--- JavaNinja/src/ProjectSpring/CadastroDeNinjas/src/main/java/dev/java10/CadastroDeNinjas/Ninjas/NinjaControllerUi.java
+++ JavaNinja/src/ProjectSpring/CadastroDeNinjas/src/main/java/dev/java10/CadastroDeNinjas/Ninjas/NinjaControllerUi.java
@@ -1,4 +1,51 @@
 package dev.java10.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/ninjas/ui")
 public class NinjaControllerUi {
+
+    private final NinjaService ninjaService;
+
+    public NinjaControllerUi(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+    @GetMapping("/listar")
+    public String listarNinjas(Model model){
+        List<NinjaDTO> ninjas = ninjaService.listarNinjas();
+        model.addAttribute("ninjas", ninjas);
+        return "listaNinjas";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarNinja(@PathVariable Long id){
+        ninjaService.deletarNinja(id);
+        return "redirect:/ninjas/ui/listar";
+    }
+
+    @GetMapping("/listar/{id}")
+    public String listarNinjaId(@PathVariable Long id, Model model){
+        NinjaDTO ninja = ninjaService.listarNinjaId(id);
+        model.addAttribute("ninja", ninja);
+        return "detalhesNinja";
+    }
+
+    @GetMapping("/adicionar")
+    public String mostrarFormularioAdicionarNinja(Model model){
+        model.addAttribute("ninja", new NinjaDTO());
+        return "criarNinja";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarNinja(@ModelAttribute NinjaDTO ninja, RedirectAttributes redirectAttributes){
+        ninjaService.adicionarNinja(ninja);
+        redirectAttributes.addFlashAttribute("mensagem", "Ninja adicionado com sucesso");
+        return "redirect:/ninjas/ui/listar";
+    }
 }
