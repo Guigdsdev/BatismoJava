ce911be4b81b970d5be7cfdf675331c42660a441
Guigdsdev <guigdsdev@gmail.com>
2025-11-09 01:32:47 -0300

    feat: Adicionado rota de UI para Ninjas e Missoes

---
+++ JavaNinja/src/ProjectSpring/CadastroDeNinjas/src/main/java/dev/java10/CadastroDeNinjas/Missoes/MissoesControllerUi.java
@@ -0,0 +1,4 @@
+package dev.java10.CadastroDeNinjas.Missoes;
+
+public class MissoesControllerUi {
+}
---
+++ JavaNinja/src/ProjectSpring/CadastroDeNinjas/src/main/java/dev/java10/CadastroDeNinjas/Ninjas/NinjaControllerUi.java
@@ -0,0 +1,4 @@
+package dev.java10.CadastroDeNinjas.Ninjas;
+
+public class NinjaControllerUi {
+}
