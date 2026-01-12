1bf0d0b6b5b6cfbdc71f18c2fd7e2f3651646fd9
Guigdsdev <guigdsdev@gmail.com>
2026-01-12 00:02:31 -0300

    feat: Adiciona FoodMapper para mapeamento entre FoodDTO e FoodModel

---
+++ JavaNinja/src/ProjectSpring/MagicFridgeAI/src/main/java/mapper/FoodMapper.java
@@ -0,0 +1,26 @@
+package mapper;
+
+import DTOs.FoodDTO;
+import model.FoodModel;
+
+public class FoodMapper {
+    public FoodModel map(FoodDTO foodDTO){
+        FoodModel foodModel = new FoodModel();
+        foodModel.setId(foodDTO.getId());
+        foodModel.setNome(foodDTO.getNome());
+        foodModel.setCategoria(foodDTO.getCategoria());
+        foodModel.setValidade(foodDTO.getValidade());
+        foodModel.setQuantidade(foodDTO.getQuantidade());
+        return foodModel;
+    }
+
+    public FoodDTO map(FoodModel foodModel){
+        FoodDTO foodDTO = new FoodDTO();
+        foodDTO.setId(foodModel.getId());
+        foodDTO.setNome(foodModel.getNome());
+        foodDTO.setCategoria(foodModel.getCategoria());
+        foodDTO.setValidade(foodModel.getValidade());
+        foodDTO.setQuantidade(foodModel.getQuantidade());
+        return foodDTO;
+    }
+}
